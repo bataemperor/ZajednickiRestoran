4dd8de4ecc1ced2cf5fd76c77be0539b788e505a
bataemperor <bataemperor@yahoo.com>
2015-07-05 21:14:17 +0200

    font

--- ServerRestoran/src/forme/FRMStavke.java
+++ ServerRestoran/src/forme/FRMStavke.java
@@ -2,6 +2,7 @@ package forme;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -55,5 +56,15 @@ public class FRMStavke extends JDialog {
 		btnOk.setBounds(369, 372, 89, 23);
 		getContentPane().add(btnOk);
 		jtblStavke.setModel(mtn);
+		jtblStavke.setFont(new Font("Arial",Font.BOLD,20));
+		jtblStavke.setRowHeight(30);
+		jtblStavke.getColumnModel().getColumn(0).setMaxWidth(200);
+		jtblStavke.getColumnModel().getColumn(0).setMinWidth(100);
+		jtblStavke.getColumnModel().getColumn(1).setMaxWidth(200);
+		jtblStavke.getColumnModel().getColumn(1).setMinWidth(100);
+		jtblStavke.getColumnModel().getColumn(2).setMinWidth(250);
+//		jtblStavke.getColumnModel().getColumn(2).
+		jtblStavke.getColumnModel().getColumn(3).setMaxWidth(200);
+		jtblStavke.getColumnModel().getColumn(3).setMinWidth(100);
 	}
 }
